52d6b9b9b57c14638492eea23b3f2ad44e2726b8
Francesco Scali <francesco.scali@gmail.com>
2019-01-13 10:20:47 +0100

    Udemy CSS class: Natours project

---
+++ src/index.d.ts
@@ -0,0 +1,19 @@
+declare module '*.png' {
+  const value: any;
+  export = value;
+}
+
+declare module '*.jpg' {
+  const value: any;
+  export = value;
+}
+
+declare module '*.webm' {
+  const value: any;
+  export = value;
+}
+
+declare module '*.mp4' {
+  const value: any;
+  export = value;
+}
---
+++ src/index.ts
@@ -0,0 +1,50 @@
+/// <reference path='./index.d.ts'/>
+import './sass/main.scss';
+//import logoWhite from './img/logo-white.png';
+//import logoGreen from './img/logo-green-2x.png';
+//import logoGreenLowRes from './img/logo-green-1x.png';
+//import nat1 from './img/nat-1-large.jpg';
+//import nat2 from './img/nat-2-large.jpg';
+//import nat3 from './img/nat-3-large.jpg';
+//import nat8 from './img/nat-8.jpg';
+
+//import nat9 from './img/nat-9.jpg';
+/*import v1 from './img/video.mp4';
+import v2 from './img/video.webm';*/
+
+/*
+ <source src="img/video.mp4" type="video/mp4">
+            <source src="img/video.webm" type="video/webm">
+*/
+
+//var logoImg: any = document.querySelector('.header__logo-box img');
+//logoImg.src = logoWhite;
+
+//var img: any = undefined; //document.querySelector('.composition__photo--p1');
+//img.src = nat1;
+
+//img = document.querySelector('.composition__photo--p2');
+//img.src = nat2;
+
+//img = document.querySelector('.composition__photo--p3');
+//img.src = nat3;
+
+/*img = document.querySelector('.story__img--1');
+img.src = nat8;
+img = document.querySelector('.story__img--2');
+img.src = nat9;*/
+
+//img = document.querySelector('.footer__logo-box img');
+//img.src = logoGreen;
+
+/*var video = document.querySelectorAll('source')[0];
+video.src = v1;
+
+video = document.querySelectorAll('source')[1];
+video.src = v2;*/
+//video.parentElement.style.width = '100%'; // legato a problemini con webpack
+/*img = document.querySelectorAll('.popup__left img')[0];
+img.src = nat8;
+
+img = document.querySelectorAll('.popup__left img')[1];
+img.src = nat9;*/
